afbb957bec2c0479ee8819320a9bd489d825377c
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-09 17:16:32 -0600

    Smooth Scroll version using cferdinandi's JS library.

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 // OPTION 2 - SMOOTH SCROLL WITH JQUERY
 // BONUS for using this method? Better cross-platform ability.
-$('.nav-bar a').on('click', function(e) {
-	//check for a hash
-	if(this.hash !== '') {
-		e.preventDefault();
+// $('.nav-bar a').on('click', function(e) {
+// 	//check for a hash
+// 	if(this.hash !== '') {
+// 		e.preventDefault();
+//
+// 		const hash = this.hash;
+//
+// 		// target body
+// 		$('html, body').animate({
+// 			// scroll down from the top, over 800ms
+// 			scrollTop: $(hash).offset().top
+// 		}, 400)
+// 	}
+// });
 
-		const hash = this.hash;
-
-		// target body
-		$('html, body').animate({
-			// scroll down from the top, over 800ms
-			scrollTop: $(hash).offset().top
-		}, 400)
-	}
+// OPTION 3 - USING THE SMOOTH SCROLL SCRIPT
+const scroll = new SmoothScroll('.nav-bar a[href*="#"]', {
+	// scroll options
+	speed: 400
 });
